808a0fc230df73dfc05ee3ce8b9598f0b59c5ae6
c3439421 <c3439421@JG109-P05>
2017-11-28 17:29:52 +0000

    Initial Commit for a fully working database example. This is to be expanded upon by adding more fields and makin the database more relevant.

--- WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -116,6 +116,7 @@
             this.Controls.Add(this.txtId);
             this.Controls.Add(this.lbxstudents);
             this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -15,11 +15,10 @@ namespace WindowsFormsApplication1
     {
         SqlConnection mySqlConnection;
 
-    
         	public void populateListBox()
     	{
        	mySqlConnection =
-            	new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\c3439421\Documents\Database1.mdf;Integrated Security=True;Connect Timeout=30 ");
+            	new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=F:\WindowsFormsApplication1\Database1.mdf;Integrated Security=True;Connect Timeout=30 ");
  
        	String selcmd = "SELECT studentId, studentName, studentAddress FROM students ORDER BY studentName";
  
@@ -103,40 +102,8 @@ namespace WindowsFormsApplication1
 
         public Form1()
         {
-
-            //    String[] myData = new String[100];
             InitializeComponent();
-
             populateListBox();
-
-            //    mySqlConnection =
-            //    new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\c3439421\Documents\Database1.mdf;Integrated Security=True;Connect Timeout=30");
-
-            //    String selcmd = "SELECT names FROM Table ";
-
-
-            //    SqlCommand mySqlCommand = new SqlCommand(selcmd, mySqlConnection);
-
-
-            //    mySqlConnection.Open();
-
-
-            //    SqlDataReader mySqlDataReader = mySqlCommand.ExecuteReader();
-
-            //    int i = 0;
-            //    while (mySqlDataReader.Read())
-            //    {
-            //        Console.WriteLine(mySqlDataReader["names"]); //reads a line of the query result at a time
-            //        myData[i++] = (String)mySqlDataReader["names"]; //store in an array too for use later
-
-            //    }
-
-            //    for (int j = 0; j < i; j++) //now iterate through our good old, bog standard array
-            //    {
-            //        Console.WriteLine("***" + myData[j] + "***");
-            //    }
-
-
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -144,12 +111,17 @@ namespace WindowsFormsApplication1
             if (checkInputs())
             {
 
-                String commandString = "INSERT INTO students(studentID, studentName, studentaddress) VALUES (@ID, @name, @address)";
+                String commandString = "INSERT INTO Students(studentID, studentName, studentaddress) VALUES (@ID, @name, @address)";
 
                 insertRecord(txtId.Text, txtName.Text, txtAddress.Text, commandString);
                 populateListBox();
                 cleartxtBoxes();
             }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
