7d4c129569c81ac5d329402d471811b9a5aecec1
c3439421 <c3439421@JG109-P05>
2017-12-05 16:27:02 +0000

    Commit for 05/12/2017

--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -110,7 +110,7 @@ namespace WindowsFormsApplication1
         {
             if (checkInputs())
             {
-
+                
                 String commandString = "INSERT INTO Students(studentID, studentName, studentaddress) VALUES (@ID, @name, @address)";
 
                 insertRecord(txtId.Text, txtName.Text, txtAddress.Text, commandString);
